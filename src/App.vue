3d47a922ad96a4c883000bd1afad5c343ced9f92
Nikita <lass245@mail.ru>
2022-12-13 00:05:50 +0300

    chore: add LeftMenu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-123
+<div class="container">
+    <LeftMenuVue/>
+    <div></div>
+</div>
 </template>
 
 <script setup lang="ts">
-
+import LeftMenuVue from './components/LeftMenu.vue';
 </script>
 
 <style scoped lang="scss">
+.container {
+    display: grid;
+    grid-template-areas: 'left-menu layouts';
+    grid-template-columns: 110px 1fr;
+    &:first-child {
+        grid-area: left-menu;
+    }
+    &:last-child {
+        grid-area: layouts;
+    }
+}
 </style>
---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,73 @@
+<template>
+    <section class="menu">
+        <div class="menu__top">
+            <div class="menu__logo">
+                <!-- <logo class="menu__log-picture" alt="logo"/> -->
+            </div>
+            <nav class="menu__nav">
+                <ul class="menu__nav-list">
+                    <li>PiZZZda</li>
+                    <li>PiZZZda</li>
+                    <li>PiZZZda</li>
+                </ul>
+            </nav>
+        </div>
+        <div class="menu__bottom">
+            <iconLogin v-if="!isAuth" @click="toggleAuth()" alt="login"/>
+            <iconExit v-if="isAuth" @click="toggleAuth()" alt="exit"/>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import iconLogin from '../assets/icons/icon-menu-login.svg?component';
+import iconExit from '../assets/icons/icon-menu-exit.svg?component';
+import logo from '../assets/logo.svg?component';
+
+const isAuth = ref(false);
+
+function toggleAuth(): void {
+    isAuth.value = !isAuth.value;
+}
+</script>
+
+<style lang="scss" scoped>
+    .menu {
+        height: 100%;
+        &__top {
+            height: 100%;
+        }
+        &__bottom {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            &-exit,
+            &-login {
+                display: block;
+                width: 24px;
+                height: 24px;
+                cursor: pointer;
+            }
+        }
+        &__logo {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 40px;
+        }
+        &__nav {
+            height: 100%;
+            &-list {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+                align-items: center;
+                height: 100%;
+                    .menu__nav-list-item {
+                        margin-bottom: 50px;
+                    }
+            }
+        }
+}
+</style>
